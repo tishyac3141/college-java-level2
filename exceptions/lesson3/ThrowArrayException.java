3c02eee11502063dec758cb732751b5aa19a68d7
Tishya Chhabra <tatooine888@gmail.com>
2020-09-12 22:07:36 -0700

    added comments for throwArrayException

--- exceptions/lesson3/ThrowArrayException.java
+++ exceptions/lesson3/ThrowArrayException.java
@@ -2,10 +2,19 @@ package exceptions.lesson3;
 
 import javax.swing.JOptionPane;
 
+/**
+ * @author: Tishya Chhabra 
+ * Date: September 12, 2020
+ * Class Info: A basic class that practices using the 'try-catch' block; prompts the user for
+ * the index of the person's name they would like to know and catches the 'IndexOutOfBounds'
+ * exception so the user knows that they provided an invalid index.
+ */
+
 public class ThrowArrayException{
 
     public static void main(String[] args){
 
+        //array of possible names
         String[] names = new String[10];
         names[0] = "Johnny";
         names[1] = "Julie";
@@ -18,9 +27,12 @@ public class ThrowArrayException{
         names[8] = "Austin";
         names[9] = "Alisha";
 
-        String input = JOptionPane.showInputDialog("Please enter the index and we'll give you a name: ");
+        //simple input that asks for the index
+        String input = JOptionPane.showInputDialog("Please enter the index and we'll give you their name: ");
         int index = Integer.parseInt(input);
 
+        //tries to access the element at that index but catches the exception thrown if
+        //that index is invalid
         try{
             JOptionPane.showMessageDialog(null, names[index]);
         } catch(ArrayIndexOutOfBoundsException exception){

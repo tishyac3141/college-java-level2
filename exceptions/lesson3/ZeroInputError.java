80bf28d080e37a3b9e3faa81492015e62fed1247
Tishya Chhabra <tatooine888@gmail.com>
2020-09-12 22:07:45 -0700

    added comments for zeroInputError

--- exceptions/lesson3/ZeroInputError.java
+++ exceptions/lesson3/ZeroInputError.java
@@ -1,8 +1,18 @@
 package exceptions.lesson3;
 
+/**
+ * @author: Tishya Chhabra 
+ * Date: September 12, 2020
+ * Class Info: A class that extends the 'Exception' class; specifically made for if 
+ * a number is divided by zero; provides a more detailed error message to display
+ * if such an operation were to happen.
+ */
+
 public class ZeroInputError extends Exception{
     
-    public String getMessage(){
+    private static final long serialVersionUID = 1L;
+
+    public String getMessage() {
         return "Error! Can't divide by zero!";
     }
 

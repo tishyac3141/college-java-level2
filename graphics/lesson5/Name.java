77fc3796b72e08a4e0ddc9089aa2bbd2dbb9e2c4
Tishya Chhabra <tatooine888@gmail.com>
2020-09-26 09:35:10 -0700

    finished basics for Name class

--- graphics/lesson5/Name.java
+++ graphics/lesson5/Name.java
@@ -1,7 +1,43 @@
 package graphics.lesson5;
 
-public class Name {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
+public class Name extends JFrame implements ActionListener{
+
+    String[] listOfNames = { "Bob", "Sally", "Mike", "Alice", "Tom" };
+    int counter = 0;
+    JButton button = new JButton(listOfNames[counter]);
     
-    
+
+    public Name() {
+        super("Name Picker!");
+        setSize(680, 420);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        setLayout(new FlowLayout());
+        
+        button.setToolTipText("Click to get a new name!");
+        button.addActionListener(this);
+        add(button);
+        
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(counter == 4){
+            counter = 0;
+        } else {
+            counter++;
+        }
+
+        button.setText(listOfNames[counter]);
+
+    }
+
+    public static void main(String[] args){
+        Name name = new Name();
+    }
+
 }

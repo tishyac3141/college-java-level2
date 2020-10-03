7b4fdf485aff453a9880c34eeb69b6e5442d9b71
Tishya Chhabra <tatooine888@gmail.com>
2020-10-02 21:15:48 -0700

    heh

--- random_code/binarySearchPractice.java
+++ random_code/binarySearchPractice.java
@@ -9,7 +9,6 @@ public class binarySearchPractice {
         String[] names = {"Alisha", "Austin", "Danna", "Sarah", "Jeanie", "Julie", "Luke"};
         String[] numbers = {"480-569-7841", "602-987-6321", "408-561-2234", "602-982-5413", "480-974-3311", "480-562-7794", "602-784-5623"};
 
-
         String name = JOptionPane.showInputDialog(null, "Type in who you would like to call:");
         JOptionPane.showMessageDialog(null, name);
     }

9b8e8ff8f8d663da26dcff5a7371990fb5e211ac
Tishya Chhabra <tatooine888@gmail.com>
2020-10-10 11:31:32 -0700

    finished Battleship

--- graphics/lesson7/Battleship.java
+++ graphics/lesson7/Battleship.java
@@ -4,21 +4,24 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Battleship extends JFrame implements MouseListener{    
+public class Battleship extends JFrame implements MouseListener{
 
     private Container cont = getContentPane();
     private GridLayout layout = new GridLayout(6, 6);
     private JButton[][] grid = new JButton[6][6];
+    private int count = 0;
+    private int row = (int)(Math.random() * 6);
 
     public Battleship(){
 
         setSize(680, 680);
         cont.setLayout(layout);
-        addMouseListener(this);
+        cont.setBackground(Color.CYAN);
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 grid[j][i] = new JButton();
+                grid[j][i].addMouseListener(this);
                 cont.add(grid[j][i]);
             }
         }
@@ -29,16 +32,12 @@ public class Battleship extends JFrame implements MouseListener{
     public void mouseClicked(MouseEvent e) {
 
         Component clicked = (e.getComponent());
-        //System.out.println(clicked);
 
         for(int i = 0; i < grid.length; i++){
-            //System.out.println("i = " + i);
             for(int j = 0; j < grid[0].length; j++){
-                //System.out.println("j = " + j);
-                //System.out.println(grid[j][i]);
-
                 if(grid[j][i].equals(clicked)){
-                    if(i == 2){
+                    if(i == row){
+                        if(!(grid[j][i].getText().equals("1"))) count++;
                         grid[j][i].setText("1");
                     } else {
                         grid[j][i].setText("0");
@@ -46,24 +45,28 @@ public class Battleship extends JFrame implements MouseListener{
                 }
             }
         }
-        
+
         setVisible(true);
+
+        if(count == 6){
+            JOptionPane.showMessageDialog(null, "You sunk a ship! Game over! ");
+            count = 0;
+            row = (int)(Math.random() * 6);
+        }
+        
 
     }
 
-    public void mousePressed(MouseEvent e) { System.out.println("mousePressed()"); }
-    public void mouseReleased(MouseEvent e) { System.out.println("mouseReleased()"); }
-    public void mouseEntered(MouseEvent e) { System.out.println("mouseEntered()"); }
-    public void mouseExited(MouseEvent e) { System.out.println("mouseExited()"); }
-    public void mouseDragged(MouseEvent e) { System.out.println("mouseDragged()"); }
-    public void mouseMoved(MouseEvent e) { System.out.println("mouseMoved()"); }
+    public void mousePressed(MouseEvent e) {  }
+    public void mouseReleased(MouseEvent e) {  }
+    public void mouseEntered(MouseEvent e) {  }
+    public void mouseExited(MouseEvent e) {  }
+    public void mouseDragged(MouseEvent e) {  }
+    public void mouseMoved(MouseEvent e) {  }
 
     public static void main(String[] args){
         Battleship ship = new Battleship();
     }
-
-    
-
-    
+     
 
 }

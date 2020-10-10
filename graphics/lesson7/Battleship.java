637ec3de8dafdd1182a2e757bd9709f663875c9f
Tishya Chhabra <tatooine888@gmail.com>
2020-10-10 10:53:29 -0700

    edited Battleship

--- graphics/lesson7/Battleship.java
+++ graphics/lesson7/Battleship.java
@@ -6,10 +6,6 @@ import java.awt.event.*;
 
 public class Battleship extends JFrame implements MouseListener{    
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
     private Container cont = getContentPane();
     private GridLayout layout = new GridLayout(6, 6);
     private JButton[][] grid = new JButton[6][6];
@@ -18,6 +14,7 @@ public class Battleship extends JFrame implements MouseListener{
 
         setSize(680, 680);
         cont.setLayout(layout);
+        addMouseListener(this);
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
@@ -30,12 +27,17 @@ public class Battleship extends JFrame implements MouseListener{
     }
     
     public void mouseClicked(MouseEvent e) {
-        Component clicked = e.getComponent();
+
+        Component clicked = (e.getComponent());
+        //System.out.println(clicked);
 
         for(int i = 0; i < grid.length; i++){
+            //System.out.println("i = " + i);
             for(int j = 0; j < grid[0].length; j++){
+                //System.out.println("j = " + j);
+                //System.out.println(grid[j][i]);
+
                 if(grid[j][i].equals(clicked)){
-                    System.out.println("true!");
                     if(i == 2){
                         grid[j][i].setText("1");
                     } else {
@@ -44,15 +46,17 @@ public class Battleship extends JFrame implements MouseListener{
                 }
             }
         }
+        
+        setVisible(true);
 
     }
 
-    public void mousePressed(MouseEvent e) { }
-    public void mouseReleased(MouseEvent e) { }
-    public void mouseEntered(MouseEvent e) { }
-    public void mouseExited(MouseEvent e) { }
-    public void mouseDragged(MouseEvent e) { }
-    public void mouseMoved(MouseEvent e) { }
+    public void mousePressed(MouseEvent e) { System.out.println("mousePressed()"); }
+    public void mouseReleased(MouseEvent e) { System.out.println("mouseReleased()"); }
+    public void mouseEntered(MouseEvent e) { System.out.println("mouseEntered()"); }
+    public void mouseExited(MouseEvent e) { System.out.println("mouseExited()"); }
+    public void mouseDragged(MouseEvent e) { System.out.println("mouseDragged()"); }
+    public void mouseMoved(MouseEvent e) { System.out.println("mouseMoved()"); }
 
     public static void main(String[] args){
         Battleship ship = new Battleship();

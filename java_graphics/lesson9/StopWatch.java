57df029f3a4baf65dc31d739f420eb919ccd9dcc
Tishya Chhabra <tatooine888@gmail.com>
2020-10-31 20:20:15 -0700

    added comments

--- java_graphics/lesson9/StopWatch.java
+++ java_graphics/lesson9/StopWatch.java
@@ -4,6 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+ * @author Tishya Chhabra
+ * Date: 10/31/2020
+ * 
+ * A basic implementation of a stopwatch! with a start and stop button, when
+ * the start button is pressed, the program keep tracks of how much time has elapsed 
+ * and when the stop button is pressed, it displays how much time passsed.
+ * Note: due to thread issues, I could not make it so that the time elapsed
+ * is constantly being updated, but I know it would be possible with multithreading.
+ */
+
 public class StopWatch extends JPanel implements ActionListener{
 
     private long timeStamp;
@@ -23,6 +34,7 @@ public class StopWatch extends JPanel implements ActionListener{
         add(stop);
         add(time);
 
+        //setting the alignment + adding a nice border to the JPanel
         setAlignmentX(Component.CENTER_ALIGNMENT);
         setBorder(BorderFactory.createTitledBorder("Stopwatch"));
 
@@ -33,12 +45,18 @@ public class StopWatch extends JPanel implements ActionListener{
         frame.setLayout(new FlowLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(this);
+        //this is something cool I found: if 'null' is passed into this method,
+        //then the frame will pop up in the center of the screen
         frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
     }
 
     public void paintComponent(Graphics g) {
+        //going through and depending on how much time has elapsed,
+        //displaying it in the proper format (for ex, if only seconds have passed, then
+        //having the "0:" in front of the number)
+        
         if(elapsedTime >= 60){
             if(elapsedTime >= 3600){
                 int hours = (int)(elapsedTime/3600);
@@ -73,16 +91,20 @@ public class StopWatch extends JPanel implements ActionListener{
     }
 
     public void start(){
+        //a message is shown so that the user knows the time actually started, since
+        //my program does not update the time elapsed in real time
         JOptionPane.showMessageDialog(null, "Your time has started! Press 'Stop' to see how much time has elapsed.");
         timeStamp = System.currentTimeMillis();
         display();
     }
 
     public void display(){
+        //updating how much time has passed before repainting
         elapsedTime = (double)((System.currentTimeMillis() - timeStamp) / 1000.0); 
         repaint();
     }
 
+    //this method is called whenever either of the buttons is pressed
     public void actionPerformed(ActionEvent e) {
         Object pressed = e.getSource();
         if(pressed.equals(start)){
--- java_graphics/lesson9/Vote.java
+++ java_graphics/lesson9/Vote.java
@@ -8,7 +8,10 @@ import java.awt.event.*;
  * @author Tishya Chhabra
  * Date: 10/30/2020
  * 
- * 
+ * A basic online voting program. With two buttons, one for each candidate, as well as a
+ * 'show results' button, as long as the program is running, it will keep track of how many 
+ * votes have been received for one candidate versus the other and will display the
+ * results after voting!  
  */
 
 public class Vote extends JFrame implements ActionListener{
@@ -25,6 +28,9 @@ public class Vote extends JFrame implements ActionListener{
         setSize(680, 400);
         setLayout(new FlowLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //this is something cool I found: if 'null' is passed into this method,
+        //then the frame will pop up in the center of the screen
+        setLocationRelativeTo(null);
 
         add(cand1);
         add(cand2);
@@ -33,30 +39,26 @@ public class Vote extends JFrame implements ActionListener{
         cand1.addActionListener(this);
         cand2.addActionListener(this);
         results.addActionListener(this);
+        
     }
 
+    //this method is called whenever either of the buttons is pressed
     public void actionPerformed(ActionEvent e) {
+        //going through and depending on what button has pressed, showing the corresponding
+        //message to the user
         Object pressed = e.getSource();
 
         if(pressed.equals(cand1)){
             numForCand1++;
             JOptionPane.showMessageDialog(null, "You voted for " + cand1.getText());
 
-            String message = "Votes for Candidate 1: " + numForCand1 + "\n" + "Votes for Candidate 2: " + numForCand2;
-            JOptionPane.showMessageDialog(null, message);
-
         } else if(pressed.equals(cand2)){
             numForCand2++;
             JOptionPane.showMessageDialog(null, "You voted for " + cand2.getText());
+        } 
 
-            String message = "Votes for Candidate 1: " + numForCand1 + "\n" + "Votes for Candidate 2: " + numForCand2;
-            JOptionPane.showMessageDialog(null, message);
-
-        } else if(pressed.equals(results)){
-            String message = "Votes for Candidate 1: " + numForCand1 + "\n" + "Votes for Candidate 2: " + numForCand2;
-            JOptionPane.showMessageDialog(null, message);
-        }
-
+        String message = "Votes for Candidate 1: " + numForCand1 + "\n" + "Votes for Candidate 2: " + numForCand2;
+        JOptionPane.showMessageDialog(null, message);
     }
 
     public static void main(String[] args){

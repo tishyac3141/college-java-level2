19ad7fe7ea63b74e895327a12b1ee6716cbed872
Tishya Chhabra <tatooine888@gmail.com>
2020-11-27 16:01:16 -0700

    finished JLottery2

--- final_project/Main.java
+++ final_project/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args){
         //JLuckySeven seven = new JLuckySeven();
 
-        JCatchTheMouse mouse = new JCatchTheMouse();
+        //JCatchTheMouse mouse = new JCatchTheMouse();
+
+        JLottery2 lottery = new JLottery2();
     }
     
 }
